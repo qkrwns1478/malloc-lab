48164ad8784f602029f808e45baab2226f683d5a
qkrwns1478 <qkrwns1478@gmail.com>
2025-04-25 21:43:17 +0900

    수정사항 반영

--- mm.c
+++ mm.c
@@ -35,15 +35,6 @@ team_t team = {
     ""
 };
 
-/* single word (4) or double word (8) alignment */
-// #define ALIGNMENT 8
-
-/* rounds up to the nearest multiple of ALIGNMENT */
-// #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
-
-
-// #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
-
 /*-----------------------------------------------------------------------------------------------*/
 /*
     Basic constants and macros for manipulating the free list
